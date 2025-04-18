e1a8e8519af2d1ce8f02bc717e14ad06d8ee7998
jairovera88@outlook.com <jairovera88@outlook.com>
2025-04-18 14:07:44 -0400

    Added a project description

--- lens_slice.py
+++ lens_slice.py
@@ -1,4 +1,8 @@
-# Your code below:
+"""
+Len's Slice
+You work at Len’s Slice, a new pizza joint in the neighborhood. You are going to use your knowledge of 
+Python lists to organize some of your sales data.
+"""
 
 # Make some pizzas
 toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
